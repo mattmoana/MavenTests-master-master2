0aa451a552c53c9d9ee9f350477a22ef257d401c
mattmoana <mattmoana@hotmail.com>
2022-12-28 22:17:45 +0200

    git innit

---
+++ src/test/java/Tests/JavaTest.java
@@ -0,0 +1,43 @@
+package Tests;
+
+import Cat.Cat;
+import Dog.Dog;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class JavaTest {
+
+    @Test
+    public void testLabradorVoiceCheck() {
+
+        Dog labrador = new Dog();
+        labrador.voice = "Woooof!";
+        assertEquals("Woooof!", labrador.voice, "It is a wrong dog voice!");
+
+    }
+
+    @Test
+    public void testCatVoiceCheck() {
+        Cat mayCoon = new Cat();
+        mayCoon.voice = "Murrr!";
+        assertEquals("Murrr!", mayCoon.voice, "Wrong cat voice!");
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/testData.csv", useHeadersInDisplayName = true)
+    public void csvFileTestSource(String Login, String Password, String Role) {
+
+        Assertions.assertNotNull(Password);
+        Assertions.assertFalse(Login.isBlank());
+        Assertions.assertNotNull(Role);
+        String csv = Login + " " + Password+ " " + Role;
+        System.out.println(csv);
+
+    }
+}
